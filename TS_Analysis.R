e863d7a40cede3761b25e399c33962cd41faea15
Manuel von Krosigk <m.vkrosigk@web.de>
2017-06-04 15:26:49 +0200

    Read Yield Curve example, preliminary manipulations, has to be adjusted

--- TS_Analysis.R
+++ TS_Analysis.R
@@ -2,40 +2,46 @@
 ##                    Thesis                      ##
 ####################################################
 
+
+# Misc --------------------------------------------------------------------
+
 # rm(list=ls())
 # setwd("C:/Users/Admin/Google Drive/Masterthesis")
 
-###
-# Read TS data
-###
-
-## 3-m T-bill rate (measure of policy innovation)
+# Read YC Data ------------------------------------------------------------
 
 # c.f. class MP Ellingsen, Söderström, p. 25
 # announcements: https://www.federalreserve.gov/feeds/h15.html
-# 3-Month Treasury Bill: Secondary Market Rate (daily)
-# source: https://fred.stlouisfed.org/series/DTB3
 
-Tbill3m                 <- read.csv(file="data/Tbill3m.csv", header=TRUE, sep=",",
-                                    na.strings =".", stringsAsFactors=FALSE)
-Date                    <- as.Date(Tbill3m[,1],"%Y-%m-%d") 
-colnames(Tbill3m)       <- c("Date","Tbill3m")
-sum(is.na(x = Tbill3m)) # NAs - obviously before and after changes
-# how to treat the NAs? -> CHECK!!
+YCGT                  <- read.csv(file="data/US_Treasury_Actives_Curve.csv", 
+                                  header=TRUE, sep=";", na.strings =".", 
+                                  stringsAsFactors=FALSE)
+YCGT[,1]              <- as.Date(YCGT[,1],"%d.%m.%Y") 
+colnames(YCGT)        <- c("Date","1M","3M","6M","1Y","2Y","3Y","5Y","7Y","10Y","30Y")
+
+# !! how to treat values missing due to holidays/weekends etc?
 # replace NAs with the last non-NA value - for instance!
-for(i in 1:nrow(Tbill3m)){
-  if(is.na(Tbill3m[i,2])==T){
-    Tbill3m[i,2]               <- Tbill3m[i-1,2]
-  }
-} 
+# !! look up first NA levels; hard paste (outch)
+YCGT[1,2] = YCGT[1,3]# noooooooooooooo
+YCGT[1,5] = (YCGT[1,4]+YCGT[1,6])/2# noooooooooooooo
+YCGT[1,9] = (YCGT[1,8]+YCGT[1,10])/2# noooooooooooooo
+# better extrapolate somehow
+for(j in 2:ncol(YCGT)){
+  for(i in 1:nrow(YCGT)){
+    if(is.na(YCGT[i,j])==T){
+      YCGT[i,j]               <- YCGT[i-1,j]
+    }
+  } 
+}
 # truncate data to relevant time period
-Tbill3m                 <- Tbill3m[which(
-                            Tbill3m[,1]=="2007-01-01"):which( # sample start
-                            Tbill3m[,1]=="2016-12-30"),]      # sample end
+YCGT                    <- YCGT[which(
+          YCGT[,1]=="2007-01-01"):which( # sample start
+          YCGT[,1]=="2016-12-31"),]      # sample end
 
-# plot(strptime(Tbill3m[,1],'%Y-%m-%d'),Tbill3m[,2],type='l',xlab="Date",ylab="3mT-bill")
+# plot(strptime(YCGT[,1],'%d-%m-%dY'),YCGT[,2],type='l',xlab="Date",ylab="1M")
 
-## Fed Interest Rate Decision
+
+# Read Target Interest Rate -----------------------------------------------
 
 # Source: https://www.federalreserve.gov/monetarypolicy/openmarket.htm
 # Source: https://www.investing.com/economic-calendar/interest-rate-decision-168
@@ -49,25 +55,24 @@ colnames(OMO)             <- c("Date","Increase (in BP)","Decrease (in BP) Min",
                          "Decrease (in BP) Max","Level (in %) Min","Level (in %) Max")
 
 # set up data frame with date from 3mTbill and corresponding interest rate targets
-Rates                     <- data.frame(Tbill3m[,1])
+Rates                     <- data.frame(YCGT[,1])
 Rates[,1]                 <- as.Date(Rates[,1])
-Rates[,2]                 <- Tbill3m[,2]
-for(j in 3:4){# fill min and max target in columns 3 & 4 (from columns 5 & 6)
+for(j in 2:3){# fill min and max target in columns 3 & 4 (from columns 5 & 6)
   for(i in 1:nrow(Rates)){
     if(!length(which(Rates[i,1]==OMO[,1]))==T){# trick when date is not included
       Rates[i,j]                 <- NA
     }
     else{
-      Rates[i,j]                 <- OMO[which(Rates[i,1]==OMO[,1]),2+j] # cols 5 & 6 
+      Rates[i,j]                 <- OMO[which(Rates[i,1]==OMO[,1]),3+j] # cols 5 & 6 
     }
   }
 }
-colnames(Rates)             <- c('Date','3mTbill','Target_min','Taget_max')
+colnames(Rates)             <- c('Date','Target_min','Taget_max')
 # replace NAs with the last value that is not NA in columns 3 & 4 of data frame
-for(j in 3:ncol(Rates)){
+for(j in 2:ncol(Rates)){
   # if first value is NA, take the one from previous year
   if(is.na(Rates[1,j])==T){
-    Rates[1,j]               <- OMO[1,2+j] # first value is from 2006, cols 5 & 6 
+    Rates[1,j]               <- OMO[1,3+j] # first value is from 2006, cols 5 & 6 
   }
   for(i in 1:nrow(Rates)){
     if(is.na(Rates[i,j])==T){
@@ -76,11 +81,11 @@ for(j in 3:ncol(Rates)){
   } 
 }
 
-plot(Rates[,1],Rates[,2],type='l',xlab="Date",ylab="Interest Rates",col='orange')
-lines(Rates[,1],Rates[,3], col='red')
-lines(Rates[,1],Rates[,4], col='cornflowerblue')
+plot(Rates[,1],Rates[,2],type='l',xlab="Date",ylab="Interest Rates",col='cornflowerblue')
+lines(Rates[,1],Rates[,3], col='cornflowerblue')
 
 # add decisions where nothing was changed for analysis!
+
 
 
 
