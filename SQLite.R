280da8d28c350324823497841ff19201d9c18939
Manuel von Krosigk <m.vkrosigk@web.de>
2017-06-13 08:25:41 +0200

    Include query for article content and title within two dates

--- SQLite.R
+++ SQLite.R
@@ -8,25 +8,33 @@ library("XLConnect")
 
 setwd("E:/20170421_MA_Data/reuters")
 
-###
-# Connect to database
-###
 
-db <- dbConnect(drv="SQLite", dbname="reuters.db")
-# db <- dbConnect(SQLite(), dbname="reuters.db")
+# Connect to database -----------------------------------------------------
+
+#db <- dbConnect(drv="SQLite", dbname="reuters.db")
+db <- dbConnect(SQLite(), dbname="reuters.db")
 
 # get a list of all tables
 alltables = dbListTables(db)
+
+# *: id, article_id, content, pub_datetime, date_scraped, title, authors, raw_data, 
+# date stuff: https://sqlite.org/lang_datefunc.html
+
 # get the Articles as a data.frame
-dbGetQuery( db,'select * from Articles limit 1' )$content # read 1st article
+#dbGetQuery(db,'SELECT * FROM Articles LIMIT 1' )$content # read 1st article
+#dbGetQuery(db, "SELECT * FROM Articles WHERE pub_datetime = '2015-01-03 23:51:00' ")
+#dbGetQuery(db, "SELECT content, title FROM Articles WHERE pub_datetime = BETWEEN date(
+#             '2015-01-03') AND date('2015-01-04') # doesn't work yet
+
+test1 <- dbGetQuery(db, "SELECT content, title FROM Articles WHERE 
+                    pub_datetime < datetime('2015-01-03 23:51:00','-1 day')")
+# alternative for later: determine ids with elastic and reference accordingly
 
 
+# Manipulate for TA -------------------------------------------------------
 
 
-
-###
-# General Queries
-###
+# General Queries ---------------------------------------------------------
 
 # connect to the sqlite file
 con = dbConnect(drv="SQLite", dbname="country.sqlite")
@@ -40,6 +48,7 @@ dbClearResult(p3)
 # Select population with managerial type of job
 p4 = dbGetQuery(con, "select * from populationtable where jobdescription like '%manager%'")
 
-###
-# End
-###
+
+# End ---------------------------------------------------------------------
+
+
