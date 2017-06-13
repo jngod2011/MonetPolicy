f9fabd3970a6e66876def222e1acee3b5cf20781
Manuel von Krosigk <m.vkrosigk@web.de>
2017-06-13 14:52:56 +0200

    Basic preprocessing for content of 20151217 OMO

--- SQLite.R
+++ SQLite.R
@@ -42,10 +42,79 @@ save(OMO_20151217,file="OMO_20151217.RData")
 # Manipulate for TA -------------------------------------------------------
 
 setwd("C:/Users/Admin/Google Drive/Masterthesis")
-load("OMO_20151217.RData")
+load("OMO_20151217.RData") # articles +/- 5 days around OMO 20151217
 min(OMO_20151217[,3]) # first article
 max(OMO_20151217[,3]) # last article 
 
+library(tm)
+title   <- Corpus(VectorSource(OMO_20151217[,2])) 
+content <- Corpus(VectorSource(OMO_20151217[,1])) 
+# inspect(title)
+writeLines(as.character(title[[23]])) # example
+writeLines(as.character(content[[23]])) # example
+
+# detect text language
+library(textcat)
+all(textcat(content)=="english") # everything in english?
+
+# part of speech tagging, see Schweinberger(2016)
+
+# create the toSpace content transformer
+toSpace <- content_transformer(function(x, pattern) {
+              return (gsub(pattern, " ", x))
+              })
+
+# eliminate non-text elements
+content <- tm_map(content, toSpace, "-") 
+content <- tm_map(content, toSpace, ":")
+content <- tm_map(content, toSpace, "'")
+content <- tm_map(content, toSpace, ",")
+content <- tm_map(content, toSpace, "/")
+content <- tm_map(content, removePunctuation)
+# strip digits (std transformation, so no need for content_transformer)
+content <- tm_map(content, removeNumbers) # numbers needed?
+# transform to lower case (need to wrap in content_transformer)
+content <- tm_map(content, content_transformer(tolower))
+# remove stopwords using the standard list in tm package
+# remove them after part of speech tagging?
+
+stpw1  <- stopwords('english')
+stpw2  <- c("ths","th","st","rd","s") # own stop words
+comn  <- unique(c(stpw1,stpw2)) # select unique stopwords
+stopwords <- unique(c(gsub("'","",comn),comn)) # final stop word list
+content <- tm_map(content, removeWords, stopwords("english"))
+
+# some weird concatenated words like "accommodativeyellen"
+
+# strip whitespace only cosmetic, rather not
+# content <- tm_map(content, stripWhitespace)
+
+# combine words that should stay together ! to be extended
+for (j in seq(docs)){
+  content[[j]] <- gsub("percentage point", "percentagepoint",content[[j]])
+  content[[j]] <- gsub("economic recovery", "economicrecovery", content[[j]])
+  content[[j]] <- gsub("janet yellen", "janetyellen", content[[j]])
+  content[[j]] <- gsub("federal reserve bank", "fed", content[[j]])
+  content[[j]] <- gsub("federal reserve", "fed", content[[j]])
+  content[[j]] <- gsub("monetary policy", "monetarypolicy", content[[j]])
+  content[[j]] <- gsub("unemployment rate", "unemploymentrate", content[[j]])
+} # does not work?
+
+# stemming -> deterministic or statistical
+# library(Rstem) # needs C/C++/Fortran
+# lemmatisation: https://cran.r-project.org/web/packages/openNLP/openNLP.pdf
+content <- tm_map(content,stemDocument)
+
+writeLines(as.character(content[[23]])) # inspect
+
+# fixing issues by hand -> find better way for that
+content <- tm_map(content, content_transformer(gsub), pattern = "organiz", replacement = "organ")
+
+
+# DTM ---------------------------------------------------------------------
+
+
+# Mining Corpus -----------------------------------------------------------
 
 
 # General Queries ---------------------------------------------------------
