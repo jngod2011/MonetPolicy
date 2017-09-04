428f09deb7a2dd99c89ba93162cd3fb9d085bf90
Manuel von Krosigk <m.vkrosigk@web.de>
2017-09-04 17:07:03 +0200

    Explain count-based function and add QE dummy to regressions

--- 04_TablesPlots.R
+++ 04_TablesPlots.R
@@ -68,7 +68,7 @@ mtext("QE3", side = 1, line = 0, outer = FALSE,
 # Endog vs Exog Days Plots ------------------------------------------------
 # c.f. p. 15ff ES paper
 
-#OMO$Classification <- Tab_Class$KNN
+#OMO$Class <- Tab_Class$KNN
 # Yield curve data only from 2002
 
 DeltaYieldCurves <- data.frame(YieldCurves[2:nrow(YieldCurves),1], 
@@ -253,8 +253,181 @@ library(xtable)
 
 # Policy Days vs Normal Days QE -------------------------------------------
 
+# QE Non-policy vs policy day regression table
+
+DeltaYieldCurves <- data.frame(YieldCurves[2:nrow(YieldCurves),1], 
+                               diff(as.matrix(YieldCurves[,2:ncol(YieldCurves)])),NA,NA,NA)
+colnames(DeltaYieldCurves) <- c("Date","d1M","d3M","d6M","d1Y","d2Y","d3Y","d5Y","d7Y",
+                                "d10Y","d20Y","d30Y","NP","P","QE")
+
+for(i in 1:nrow(DeltaYieldCurves)){# create policy day dummy
+  if(length(which(OMO[,1]==DeltaYieldCurves[i,1])) == 0)
+    DeltaYieldCurves$P[i]                       <- 0
+  else
+    DeltaYieldCurves$P[i]                       <- 1
+}
+DeltaYieldCurves$NP  <- 1-DeltaYieldCurves$P
+
+# QE dates
+QE_dates <- c(
+  "2008-11-25", # QE1
+  #("2009-03-18, # QE1)
+  "2010-11-03", # QE2
+  "2011-09-21", # Op Twist
+  "2012-09-13") # QE3
+
+for(i in 1:nrow(DeltaYieldCurves)){# create QE dummy
+  if(length(which(QE_dates==DeltaYieldCurves[i,1])) == 0)
+    DeltaYieldCurves$QE[i]                       <- 0
+  else
+    DeltaYieldCurves$QE[i]                       <- 1
+}
+
+# Regression and fill table
+Tab_NPvsPdays_QE <- matrix(nrow=10-1-1+2,ncol=10)
+library(car)
+Tab_NPvsPdays_QE[,1] <- c("$\\alpha_n$", "", "$\\beta_n^{NP}$", "", "$\\beta_n^P$", "",
+                          "$\\beta_n^{QE}$", "",
+                       "$\\bar{R}^2$","$\\beta_n^{NP}$ = $\\beta_n^P$")
+colnames(Tab_NPvsPdays_QE) <- c(" ","6m","1y","2y","3y","5y","7y","10y","20y","30y")
+
+for(k in 4:(ncol(DeltaYieldCurves)-3)){
+  # regress change in n-maturity on NP-dummy*delta3m and P-dummy*delta3m,
+  # control for QE    
+  myreg <- lm(DeltaYieldCurves[,k]~DeltaYieldCurves$d3M:DeltaYieldCurves$NP + 
+                DeltaYieldCurves$d3M:DeltaYieldCurves$P+
+                DeltaYieldCurves$QE)
+  # fill table
+  j = k+1-3 
+  Tab_NPvsPdays_QE[1,j]  <- formatC(abs(round(summary(myreg)$coef[1,1],2)),format="f",digits=2) # intercept
+  Tab_NPvsPdays_QE[2,j]  <- paste0("(", format(unlist(
+    formatC(abs(round(summary(myreg)$coef[1,4],2)),format="f",digits=2)
+  )),")") # std intercept
+  Tab_NPvsPdays_QE[3,j]  <- round(summary(myreg)$coef[3,1],2) # beta NP
+  Tab_NPvsPdays_QE[4,j]  <- paste0("(", format(unlist(
+    formatC(abs(round(summary(myreg)$coef[3,4],2)),format="f",digits=2)
+  )),")") # std beta NP
+  Tab_NPvsPdays_QE[5,j]  <- round(summary(myreg)$coef[4,1],2) # beta P
+  Tab_NPvsPdays_QE[6,j]  <- paste0("(", format(unlist(
+    formatC(abs(round(summary(myreg)$coef[4,4],2)),format="f",digits=2)
+  )),")") # std beta P
+  Tab_NPvsPdays_QE[7,j]  <- round(summary(myreg)$coef[2,1],2) # beta QE
+  Tab_NPvsPdays_QE[8,j]  <- paste0("(", format(unlist(
+    formatC(abs(round(summary(myreg)$coef[2,4],2)),format="f",digits=2)
+  )),")") # std beta QE
+  Tab_NPvsPdays_QE[9,j]  <- round(summary(myreg)$r.squared,2) # R^2
+  # add D-W statistic?
+  Tab_NPvsPdays_QE[10,j]  <- formatC(abs(round(linearHypothesis(myreg,
+                                                            "DeltaYieldCurves$d3M:DeltaYieldCurves$NP=DeltaYieldCurves$d3M:DeltaYieldCurves$P")$Pr[2
+                                                                                                                                                   ],2)),format="f",digits=2)
+}
+
+# export table to latex format
+library(xtable)
+#print(xtable(Tab_NPvsPdays_QE, align="lrrrrrrrrrr", digits=2, type="latex", 
+#             caption="Yield curve response to short rate movements on policy days and non-policy days as well as QE announcements.",
+#             label = "tab:NPvsPdays_QE"), 
+#      sanitize.text.function = function(x){x}, include.rownames=F,
+#      booktabs=TRUE, caption.placement="top", size="\\small",
+#      file="Text/chapters/tables_graphs/NPvsPdays_QE.tex")
+
 
 # Exog vs Endog QE Reg ----------------------------------------------------
+
+# QE endog vs exog day regression table
+Tab_EndvsExdays_QE  <- matrix(nrow=14-1-1-1+2,ncol=10)
+library(car)
+Tab_EndvsExdays_QE[,1] <- c("$\\alpha_n$", "", "$\\beta_n^{NP}$", "", "$\\beta_n^{End}$", "",
+                          "$\\beta_n^{Ex}$", "", "$\\beta_n^{QE}$", "", "$\\bar{R}^2$", 
+                         "$\\beta_n^{NP}$ = $\\beta_n^{End}$", "$\\beta_n^{End}$ = $\\beta_n^{Ex}$")
+colnames(Tab_EndvsExdays_QE) <- c(" ","6m","1y","2y","3y","5y","7y","10y","20y","30y")
+
+# yield curve regressor
+DeltaYieldCurves <- data.frame(YieldCurves[2:nrow(YieldCurves),1], 
+                               diff(as.matrix(YieldCurves[,2:ncol(YieldCurves)])),NA,NA,NA,NA)
+colnames(DeltaYieldCurves) <- c("Date","d1M","d3M","d6M","d1Y","d2Y","d3Y","d5Y","d7Y",
+                                "d10Y","d20Y","d30Y", "NP", "End","Ex","QE")
+
+DeltaYieldCurves[,ncol(DeltaYieldCurves)-1]  <- 1-DeltaYieldCurves[,ncol(DeltaYieldCurves)]
+
+# fill in end or ex classification into regressor table
+for(i in 1:nrow(DeltaYieldCurves)){
+  if(length(which(OMO$Date==DeltaYieldCurves$Date[i])) == 0){
+    DeltaYieldCurves$NP[i]    <- 1
+    DeltaYieldCurves$End[i]   <- 0
+    DeltaYieldCurves$Ex[i]    <- 0
+  }
+  else{
+    if(OMO$Class[which(OMO$Date==DeltaYieldCurves$Date[i])]=="Endog"){
+      DeltaYieldCurves$NP[i]    <- 0
+      DeltaYieldCurves$End[i]   <- 1
+      DeltaYieldCurves$Ex[i]    <- 0
+    }
+    if(OMO$Class[which(OMO$Date==DeltaYieldCurves$Date[i])]=="Exog"){
+      DeltaYieldCurves$NP[i]    <- 0      
+      DeltaYieldCurves$End[i]   <- 0
+      DeltaYieldCurves$Ex[i]    <- 1
+    }
+  }
+}
+
+for(i in 1:nrow(DeltaYieldCurves)){# create QE dummy
+  if(length(which(QE_dates==DeltaYieldCurves[i,1])) == 0)
+    DeltaYieldCurves$QE[i]                       <- 0
+  else
+    DeltaYieldCurves$QE[i]                       <- 1
+}
+
+# regression and fill into table
+for(k in 4:(ncol(DeltaYieldCurves)-4)){
+  # regress change in n-maturity on NP-dummy*delta3m + End-dummy*delta3m + Ex-dummy*delta3m
+  # + QE dummy
+  myreg <- lm(DeltaYieldCurves[,k]~DeltaYieldCurves$d3M:DeltaYieldCurves$NP + 
+                DeltaYieldCurves$d3M:DeltaYieldCurves$End+
+                DeltaYieldCurves$d3M:DeltaYieldCurves$Ex+
+                DeltaYieldCurves$QE)
+  # fill table
+  j = k+1-3 
+  Tab_EndvsExdays_QE[1,j]  <- formatC(abs(round(summary(myreg)$coef[1,1],2)),format="f",digits=2) # intercept
+  Tab_EndvsExdays_QE[2,j]  <- paste0("(", format(unlist(
+    formatC(abs(round(summary(myreg)$coef[1,4],2)),format="f",digits=2)
+  )),")") # std intercept
+  Tab_EndvsExdays_QE[3,j]  <- round(summary(myreg)$coef[3,1],2) # beta NP
+  Tab_EndvsExdays_QE[4,j]  <- paste0("(", format(unlist(
+    formatC(abs(round(summary(myreg)$coef[3,4],2)),format="f",digits=2)
+  )),")") # std beta NP
+  Tab_EndvsExdays_QE[5,j]  <- round(summary(myreg)$coef[4,1],2) # beta End
+  Tab_EndvsExdays_QE[6,j]  <- paste0("(", format(unlist(
+    formatC(abs(round(summary(myreg)$coef[4,4],2)),format="f",digits=2)
+  )),")") # std beta End
+  Tab_EndvsExdays_QE[7,j]  <- round(summary(myreg)$coef[5,1],2) # beta Ex
+  Tab_EndvsExdays_QE[8,j]  <- paste0("(", format(unlist(
+    formatC(abs(round(summary(myreg)$coef[5,4],2)),format="f",digits=2)
+  )),")") # std beta Ex
+  Tab_EndvsExdays_QE[9,j]  <- round(summary(myreg)$coef[2,1],2) # beta QE
+  Tab_EndvsExdays_QE[10,j]  <- paste0("(", format(unlist(
+    formatC(abs(round(summary(myreg)$coef[2,4],2)),format="f",digits=2)
+  )),")") # std beta QE
+  Tab_EndvsExdays_QE[11,j]  <- round(summary(myreg)$r.squared,2) # R^2
+  # add D-W statistic?
+  Tab_EndvsExdays_QE[12,j]  <- formatC(abs(round(linearHypothesis(myreg, # test equality param NP & End
+                                                               "DeltaYieldCurves$d3M:DeltaYieldCurves$NP=DeltaYieldCurves$d3M:DeltaYieldCurves$End")$Pr[2
+                                                                                                                                                        ],2)),format="f",digits=2)
+  Tab_EndvsExdays_QE[13,j]  <- formatC(abs(round(linearHypothesis(myreg, # test equality param End & Ex
+                                                               "DeltaYieldCurves$d3M:DeltaYieldCurves$End=DeltaYieldCurves$d3M:DeltaYieldCurves$Ex")$Pr[2
+                                                                                                                                                        ],2)),format="f",digits=2)
+}
+
+# export table to latex format
+library(xtable)
+#print(xtable(Tab_EndvsExdays_QE, align="lrrrrrrrrrr", digits=2, type="latex", 
+#             caption="Yield curve response to short rate movements on classified policy days after controlling for QE announcements.",
+#             label = "tab:EndogvsExogdays_QE"), 
+#      sanitize.text.function = function(x){x}, include.rownames=F,
+#      booktabs=TRUE, caption.placement="top", size="\\small",
+#      file="Text/chapters/tables_graphs/EndogvsExogdays_QE.tex")
+
+
 
 
 ####################################################
